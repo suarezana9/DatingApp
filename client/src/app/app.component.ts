74e2848c9710a298b4e020d46891e22aecbf0550
Ana Suarez <ana.suarez@cloud.studio>
2021-10-12 09:09:33 -0300

    created a home and register component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { User } from './_models/user';
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
@@ -11,23 +12,16 @@ export class AppComponent implements OnInit {
   users: any;
 
   constructor(
-    private http: HttpClient
+    private accountService: AccountService
   ) {}
   
   ngOnInit() {
-    this.getUsers();
+    this.setCurrentUser();
   }
   
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(
-      res => {
-        this.users = res;
-      },
-      err => {
-        console.log(err, "error");
-        
-      }
-    )
+  setCurrentUser() {
+    const user: User = JSON.parse(localStorage.getItem('user') || '{}');
+    this.accountService.setCurrenteUser(user);
   }
 }
 
